19e6ca8e7fed5745c26d1c5c72e1603ade4b0ea8
YourName <you@example.com>
2025-07-31 17:53:34 +0530

    Fix static + bot ready

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, jsonify
 import json
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 ORDERS_FILE = 'orders.json'
 
 def load_orders():
